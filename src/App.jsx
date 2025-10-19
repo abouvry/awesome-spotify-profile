123f3d4374466ff3b9673c19dd840b9f9d692102
Alexis Bouvry <bouvryalexis@gmail.com>
2025-10-19 23:49:36 +0200

    create route and access token

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import { useState } from "react";
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/Login";
-import ProfilePage from "./pages/Profile";
+import React, { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import { getAccessToken } from "./utils/auth";
 
-function App() {
+export default function App() {
+  const [token, setToken] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+  const query = new URLSearchParams(window.location.search);
+  const code = query.get("code");
+
+  if (code && !localStorage.getItem("spotify_token")) {
+    getAccessToken(code).then((token) => {
+      setToken(token);
+      navigate("/profile");
+    });
+  } else {
+    const savedToken = localStorage.getItem("spotify_token");
+    if (savedToken) setToken(savedToken);
+  }
+}, [navigate]);
+
+  const logout = () => {
+    setToken("");
+    window.localStorage.removeItem("spotify_token");
+    navigate("/");
+  };
 
   return (
     <div className="min-h-screen bg-neutral-50 text-neutral-900 p-6">
       <Routes>
-        <Route path="/" element={<LoginPage token={token} />} />
+        <Route path="/" element={<Login token={token} />} />
         <Route
           path="/profile"
-          element={<ProfilePage token={token} onLogout={logout} />}
+          element={<Profile token={token} onLogout={logout} />}
         />
       </Routes>
     </div>
   );
 }
-
-export default App;
